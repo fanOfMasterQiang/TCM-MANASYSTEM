49eea13fbebbbc981f7519ed96ff212b424970a6
john <2278222765@qq.com>
2019-04-02 09:27:27 +0800

    删除

--- config/router.config.js
+++ config/router.config.js
@@ -5,7 +5,7 @@ export default [
     component: '../layouts/UserLayout',
     routes: [
       { path: '/user', redirect: '/user/login' },
-      { path: '/user/login', name: 'login', component: './User/Login' },
+      { path: '/user/login', name: 'login', component: './Login/Login' },
       {
         component: '404',
       },
